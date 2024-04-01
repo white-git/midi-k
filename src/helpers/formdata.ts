462084ac16c2545d1ef32aa20ea44041fb7cf098
Moises Suarez Vargas <moisesalberto3@gmail.com>
2024-04-01 03:09:50 -0600

    add: helpers folder to the src, remove it from core

---
+++ src/helpers/formdata.ts
@@ -0,0 +1,5 @@
+export function formdataToObject<T>(formdata: FormData) {
+  return <T>Array
+    .from(formdata.keys())
+    .reduce((obj: Indexable, k: string) => (obj[k] = formdata.get(k), obj), {})
+}
