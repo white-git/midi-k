44c833f21d73f0abce7655c36166d40db16fd200
Moises Suarez Vargas <moisesalberto3@gmail.com>
2024-06-01 01:28:58 -0600

    add: libnut library

--- libnut/main.js
+++ libnut/main.js
@@ -1,16 +1,10 @@
-const { keyboard, Key } = require('@nut-tree/nut-js')
+const { keyboard, Key } = require('./nut-tree-js/nut-js')
 
 module.exports = class Automation {
-  /**
-   * @param {number} delay - in milliseconds.
-   */
   setDelay(delay) {
     keyboard.config.autoDelayMs = delay
   }
 
-  /**
-   * @param {Key[]} keys - reference to nut.js Key enum.
-   */
   async sendKey(keys) {
     await keyboard.pressKey(...keys)
     await keyboard.releaseKey(...keys)
